fb472032d6345cefbbdcb48d3b50b3f23ab57f26
Raju <balasadi023@gmail.com>
2023-09-12 20:44:50 -0700

    fixed secrets

--- probeq/settings.py
+++ probeq/settings.py
@@ -141,11 +141,3 @@ MEDIA_URL = '/uploads/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ramaraju367@email.com'
-EMAIL_HOST_PASSWORD = 'Symantec@8080'
-DEFAULT_FROM_EMAIL = 'ramaraju367@email.com'
